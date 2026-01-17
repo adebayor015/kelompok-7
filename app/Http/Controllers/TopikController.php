c76b834b9694e77dd798b00872bd9cf1d74cdace
ucii26 <uciinentoo@outlook.com>
2026-01-17 18:39:29 +0700

    Perbaiki tampilan kategori di halaman topik

--- app/Http/Controllers/TopikController.php
+++ app/Http/Controllers/TopikController.php
@@ -2,10 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class TopikController extends Controller
 {
     public function index()
     {
-        return view('topik');
+        $videos = [
+            [
+                'title' => 'Pembahasan Soal Volume Tabung',
+                'youtube_id' => 'a7maYlxsCcI',
+                'mapel' => 'Matematika'
+            ],
+            [
+                'title' => 'Cara Cepat Pecahan',
+                'youtube_id' => 'u5NYroWSWj8',
+                'mapel' => 'Matematika'
+            ],
+            [
+                'title' => 'Persamaan Linear',
+                'youtube_id' => 'wJcs0ZzNq4U',
+                'mapel' => 'Matematika'
+            ],
+        ];
+
+        $kategoris = ['Matematika', 'IPA', 'IPS', 'Sains', 
+        'Bahasa Indonesia', 'Bahasa Inggris', 'Sejarah', 'PPKN', 'Agama'];
+        return view('topik', compact('videos', 'kategoris'));
     }
 }
