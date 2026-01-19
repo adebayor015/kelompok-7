380b58821314cf3037c82fbff0d9e7702b30c356
kukuhpradiyantobekti <kukuhpradiyantobekti@gmail.com>
2026-01-19 11:55:59 +0700

    logo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,7 +11,7 @@
     <nav class="bg-white shadow-md sticky top-0 z-10">
         <div class="max-w-7xl mx-auto px-4 py-3 flex justify-between items-center">
             <div class="flex items-center space-x-2">
-                <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="KRFSM Logo" class="h-8">
+                <img src="{{ asset('images/krfsmrp.png') }}" alt="KRFSM Logo" class="h-20">
                 <span class="font-semibold text-xl text-blue-600">KRFSM</span>
             </div>
             <div class="hidden md:flex space-x-6 text-sm font-medium">
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -33,7 +33,7 @@
     <nav class="bg-white shadow-md sticky top-0 z-10">
         <div class="max-w-7xl mx-auto px-4 py-3 flex justify-between items-center">
             <div class="flex items-center space-x-2">
-                <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="KRFSM Logo" class="h-8">
+                <img src="{{ asset('images/krfsmrp.png') }}" alt="KRFSM Logo" class="h-20">
                 <span class="font-semibold text-xl text-blue-600">KRFSM</span>
             </div>
 
