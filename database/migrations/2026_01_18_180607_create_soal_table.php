361db43b2760b3cd5dd1a36278f21ad9fa686b56
ucii26 <uciinentoo@outlook.com>
2026-01-20 19:30:00 +0700

    sucii

--- database/migrations/2026_01_18_180607_create_soal_table.php
+++ database/migrations/2026_01_18_180607_create_soal_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('soal', function (Blueprint $table) {
             $table->id();
+            $table->enum('type', ['video', 'artikel', 'diskusi']);
+            $table->string('judul');
+            $table->text('konten');
             $table->timestamps();
         });
     }
